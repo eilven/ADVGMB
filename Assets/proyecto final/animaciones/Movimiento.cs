7bd6283b94e81d8342457866d77a7537f1c3bb7c
eilven <78859973+eilven@users.noreply.github.com>
2021-05-23 22:34:23 +0200

    final
    acabado

---
+++ Assets/proyecto final/animaciones/Movimiento.cs
@@ -0,0 +1,67 @@
+﻿using System.Runtime.CompilerServices;
+using System.Globalization;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Threading;
+using System.Numerics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+   
+    public CharacterController charController;
+    Animator animator;
+    public float speed = 10;
+    public float Downaccel = 100;
+   public float _speed = 10;
+   public float _rotSpeed= 100;
+  // private Vector3 rotation;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        charController = GetComponent<CharacterController>();
+        
+    }
+
+    // Update is called once per frame
+  
+    void Update()
+    {
+        float H = Input.GetAxis("Horizontal");
+        float V = Input.GetAxis("Vertical");
+      //  this.rotation = new Vector3(0, H*_rotSpeed*time.deltaTime, 0);
+        
+    //    charController.Move(Vector3.down* Downaccel*Timeout.deltaTime);
+       animator.SetFloat("vertical", 0);
+       animator.SetFloat("horizontal",0);
+
+    if(Input.GetKey(KeyCode.Y)){
+        animator.SetFloat("vertical", 1);
+        animator.SetFloat("horizontal", 1);
+        
+
+    }
+      if(Input.GetKey(KeyCode.G)){
+        animator.SetFloat("vertical", 0);
+        animator.SetFloat("horizontal", -1);
+        
+
+    }
+ if(Input.GetKey(KeyCode.J)){
+        animator.SetFloat("vertical", 0);
+        animator.SetFloat("horizontal", 1);
+        
+
+    }
+
+
+
+
+        
+    }
+}
---
+++ Assets/proyecto final/animaciones/movimientoIndirecto.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimientoIndirecto : MonoBehaviour
+{
+    //NavMeshAgent variable control player movement
+    public UnityEngine.AI.NavMeshAgent playerNavMeshAgent;
+
+    //A Camera that follow player movement
+    public Camera playerCamera;
+
+    //Control the animation clips of player object 
+    public Animator playerAnimator;
+    public Animator tumbaAnimator;
+
+    //check player is running(moving) or not
+    public bool isRunning;
+
+    private void Update()
+
+    {
+
+        //if the left button of is clicked
+        if (Input.GetMouseButton(0))
+        {
+            //Unity cast a ray from the position of mouse cursor on-screen toward the 3D scene.
+            Ray myRay = playerCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit myRaycastHit;
+
+            if (Physics.Raycast(myRay, out myRaycastHit))
+            {
+
+                //Assign ray hit point as Destination of Navemesh Agent (Player)
+                playerNavMeshAgent.SetDestination(myRaycastHit.point);
+            }
+        }
+
+        //Compare the value of the remaining distance and the stopping distance(Destination point)
+
+        if (playerNavMeshAgent.remainingDistance <= playerNavMeshAgent.stoppingDistance)
+        {
+            //The remaining distance are less or equal than the stopping distance it means character stop moving and reached destination
+            isRunning = false;
+        }
+        else
+        {
+            //If remaining distance are greater than the stopping distance than character still moving toward Destination
+            isRunning = true;
+        }
+
+        playerAnimator.SetBool("IsRun", isRunning);
+        while (isRunning)
+        {
+           
+
+          //  tumbaAnimator.SetBool("roto", isRunning);
+            isRunning = false;
+
+        }
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        print("dong");
+        print(other.gameObject.name);
+        Destroy(other.gameObject);
+        if (other.gameObject.name == "tomb_Shape1")
+        {
+            
+            print("soy 1");
+            Destroy(other.gameObject);
+
+            // tumbaAnimator.SetBool("roto", true);
+        }
+        if (other.gameObject.name == "tomb_Shape1 (4)")
+        {
+            print("winner");
+           
+            // tumbaAnimator.SetBool("roto", true);
+        }
+      
+        
+    }
+    }
+   
+
